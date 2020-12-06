1d2aad785d659af0c9330605638a17dbfc895814
tamnguyen90 <huta.nguyen@gmail.com>
2020-12-06 13:52:21 -0600

    Update security config for access H2 console

--- src/main/java/edu/miu/cs/neptune/config/SpringSecurityConfiguration.java
+++ src/main/java/edu/miu/cs/neptune/config/SpringSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package edu.miu.cs.neptune.config;
 
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/h2-console/**").permitAll()
+                .and().formLogin();
+
+        //Those two settings below is to enable access h2 database via browser
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+    }
 }
