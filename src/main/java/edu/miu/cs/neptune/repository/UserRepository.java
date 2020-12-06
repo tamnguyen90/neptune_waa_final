4e6b35356d337b9c40346c8fa0f60b61d5d2eee9
Truong Thanh Nam Nguyen <namco2011@gmail.com>
2020-12-06 13:35:04 -0600

    update

--- src/main/java/edu/miu/cs/neptune/repository/UserRepository.java
+++ src/main/java/edu/miu/cs/neptune/repository/UserRepository.java
@@ -2,6 +2,10 @@ package edu.miu.cs.neptune.repository;
 
 import edu.miu.cs.neptune.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
