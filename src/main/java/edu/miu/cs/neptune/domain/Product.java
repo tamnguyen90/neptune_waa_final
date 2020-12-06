63ca74824b3eeb532ee8cf45c28548a1fb5735b8
ganzorigu <ganzo.ulziibayar@gmail.com>
2020-12-06 17:17:02 -0600

    Image.java class is created. which stores image information product.java class has one to many relationship with Image

--- src/main/java/edu/miu/cs/neptune/domain/Product.java
+++ src/main/java/edu/miu/cs/neptune/domain/Product.java
@@ -1,5 +1,92 @@
 package edu.miu.cs.neptune.domain;
 
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "product")
 public class Product {
     //GANZO
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+
+    @OneToMany
+    private List<Image> imageList = new ArrayList<>();
+
+    private String productDescription;
+    private String sellerId;
+    private ProductStatus productStatus;
+
+    @OneToOne
+    private User seller;
+    private LocalDateTime paymentDueDate;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public List<Image> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(List<Image> imageList) {
+        this.imageList = imageList;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public ProductStatus getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(ProductStatus productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public LocalDateTime getPaymentDueDate() {
+        return paymentDueDate;
+    }
+
+    public void setPaymentDueDate(LocalDateTime paymentDueDate) {
+        this.paymentDueDate = paymentDueDate;
+    }
 }
