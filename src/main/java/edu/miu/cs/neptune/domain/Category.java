3c574478010e6dfa7d0bf5e4985993db951f583b
nvhbao <nvhbao@gmail.com>
2020-12-06 11:46:40 -0600

    Initialize Category class.

--- src/main/java/edu/miu/cs/neptune/domain/Category.java
+++ src/main/java/edu/miu/cs/neptune/domain/Category.java
@@ -1,5 +1,19 @@
 package edu.miu.cs.neptune.domain;
 
-public class Category {
-    //GANZO
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    private String categoryName;
+    private String categoryDescription;
 }
