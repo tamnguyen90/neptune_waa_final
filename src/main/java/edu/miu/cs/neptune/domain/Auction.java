1f8a2b3ebb6978e9ad3bbbdf3389f562cabb2685
nvhbao <nvhbao@gmail.com>
2020-12-06 12:03:50 -0600

    Initialize Auction class (including two enum classes: AuctionStatus & ShippingStatus)

--- src/main/java/edu/miu/cs/neptune/domain/Auction.java
+++ src/main/java/edu/miu/cs/neptune/domain/Auction.java
@@ -1,5 +1,25 @@
 package edu.miu.cs.neptune.domain;
 
-public class Auction {
-    //BAO
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Auction implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long auctionId;
+    private LocalDateTime beginDate;
+    private LocalDateTime endDate;
+    private AuctionStatus auctionStatus;
+    private Double beginPrice;
+    private Long winnerBidId;
+    private List<Bid> bidId;
+    private LocalDateTime shippingDate;
+    private ShippingStatus shippingStatus;
 }
