84205a2d29f105004d083a040210d9debb66142b
tamnguyen90 <huta.nguyen@gmail.com>
2020-12-06 14:03:21 -0600

    - Model declaration : Admin, Bid

--- src/main/java/edu/miu/cs/neptune/domain/Bid.java
+++ src/main/java/edu/miu/cs/neptune/domain/Bid.java
@@ -1,8 +1,68 @@
 package edu.miu.cs.neptune.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
+@Table(name = "bid")
 public class Bid {
-    //TAM
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "amount")
+    private Double biddingAmount;
+
+    @Column(name = "bidding_time")
+    private LocalDateTime biddingTime;
+
+    public Bid() {}
+
+    public Bid(Double biddingAmount, LocalDateTime biddingTime) {
+        this.biddingAmount = biddingAmount;
+        this.biddingTime = biddingTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Double getBiddingAmount() {
+        return biddingAmount;
+    }
+
+    public void setBiddingAmount(Double biddingAmount) {
+        this.biddingAmount = biddingAmount;
+    }
+
+    public LocalDateTime getBiddingTime() {
+        return biddingTime;
+    }
+
+    public void setBiddingTime(LocalDateTime biddingTime) {
+        this.biddingTime = biddingTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bid bid = (Bid) o;
+        return id.equals(bid.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Bid{" +
+                "id=" + id +
+                ", biddingAmount=" + biddingAmount +
+                ", biddingTime=" + biddingTime +
+                '}';
+    }
 }
