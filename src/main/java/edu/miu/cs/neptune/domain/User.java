dbbd978fbf9b28a29658e5d3c87a4cd1b6a4a2db
Truong Thanh Nam Nguyen <namco2011@gmail.com>
2020-12-06 11:56:15 -0600

    update

--- src/main/java/edu/miu/cs/neptune/domain/User.java
+++ src/main/java/edu/miu/cs/neptune/domain/User.java
@@ -10,4 +10,75 @@ public class User {
     private ProfileVerificationType profileVerificationType;
     private Boolean isResetPassword;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public User setUserId(Long userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public User setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+        return this;
+    }
+
+    public ProfileVerificationType getProfileVerificationType() {
+        return profileVerificationType;
+    }
+
+    public User setProfileVerificationType(ProfileVerificationType profileVerificationType) {
+        this.profileVerificationType = profileVerificationType;
+        return this;
+    }
+
+    public Boolean getResetPassword() {
+        return isResetPassword;
+    }
+
+    public User setResetPassword(Boolean resetPassword) {
+        isResetPassword = resetPassword;
+        return this;
+    }
 }
