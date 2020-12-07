a737bd41f815ecd49a3d78b665d9b6bbdff7a335
tamnguyen90 <huta.nguyen@gmail.com>
2020-12-07 11:04:16 -0600

    - Update some models, and init bidding system

---
+++ src/main/java/edu/miu/cs/neptune/service/impl/AuctionServiceImpl.java
@@ -0,0 +1,8 @@
+package edu.miu.cs.neptune.service.impl;
+
+import edu.miu.cs.neptune.service.AuctionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuctionServiceImpl implements AuctionService {
+}
---
+++ src/main/java/edu/miu/cs/neptune/service/impl/ShippingServiceImpl.java
@@ -0,0 +1,8 @@
+package edu.miu.cs.neptune.service.impl;
+
+import edu.miu.cs.neptune.service.ShippingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingServiceImpl implements ShippingService {
+}
