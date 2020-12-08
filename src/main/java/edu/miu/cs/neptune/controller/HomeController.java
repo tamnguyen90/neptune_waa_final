34b30f4f9f1ae153c524e8b63c0be3ceb564a57c
Truong Thanh Nam Nguyen <namco2011@gmail.com>
2020-12-07 19:23:55 -0600

    Create default testing user: user/1234

--- src/main/java/edu/miu/cs/neptune/controller/HomeController.java
+++ src/main/java/edu/miu/cs/neptune/controller/HomeController.java
@@ -7,10 +7,7 @@ import edu.miu.cs.neptune.service.UserVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -38,6 +35,11 @@ public class HomeController {
         else {
             return "login";
         }
-
     }
+
+    @RequestMapping("/verification")
+    public String returnVerificationPage() {
+        return "verification";
+    }
+
 }
