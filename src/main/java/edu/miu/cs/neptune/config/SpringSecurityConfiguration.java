fecd7007b419afb7db8e1fd7a129159e8f751640
tamnguyen90 <huta.nguyen@gmail.com>
2020-12-13 11:28:54 -0600

    - some issues fixed

--- src/main/java/edu/miu/cs/neptune/config/SpringSecurityConfiguration.java
+++ src/main/java/edu/miu/cs/neptune/config/SpringSecurityConfiguration.java
@@ -55,8 +55,8 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/product/**", "/auction/**").hasRole("SELLER")
-                .antMatchers("/auction/**").hasRole("BUYER")
+                .antMatchers("/product/**").hasRole("SELLER")
+                .antMatchers("/auction/**").hasAnyRole("BUYER", "SELLER")
                 .antMatchers("/users/create","/users/forgotPassword","/users/accessDenied","/users/resetPasswordVerification","/users/resetPassword","/login", "/h2-console").permitAll()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
