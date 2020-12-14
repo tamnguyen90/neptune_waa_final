950da8b7a086f7336f9d8a16cfea7fb7173adc83
nvhbao <nvhbao@gmail.com>
2020-12-13 22:03:25 -0600

    Seller - fix issue "can't delete product" by writing DELETE query.

--- src/main/java/edu/miu/cs/neptune/repository/AuctionRepository.java
+++ src/main/java/edu/miu/cs/neptune/repository/AuctionRepository.java
@@ -3,6 +3,8 @@ package edu.miu.cs.neptune.repository;
 import edu.miu.cs.neptune.domain.Auction;
 import edu.miu.cs.neptune.domain.AuctionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,8 @@ import java.util.List;
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
     List<Auction> findAllByAuctionStatus(AuctionStatus status);
     List<Auction> findAllByAuctionStatusAndEndDateAfter(AuctionStatus status, LocalDateTime now);
+
+    @Modifying
+    @Query(value = "DELETE FROM Auction a WHERE a.auctionId = :auctionId")
+    void deleteById(Long auctionId);
 }
